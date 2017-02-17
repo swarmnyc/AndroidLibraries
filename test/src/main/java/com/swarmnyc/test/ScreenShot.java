9d330291c022b2f2a7ac9736fbd4cba99b567c6e
Wade <wadehuang@gmail.com>
2017-02-17 12:53:59 -0500

    Add Thread Safe Screenshot

--- test/src/main/java/com/swarmnyc/test/ScreenShot.java
+++ test/src/main/java/com/swarmnyc/test/ScreenShot.java
@@ -15,13 +15,13 @@ import java.io.OutputStream;
 import java.util.Date;
 import java.util.UUID;
 
-public class ScreenShot {
+public class Screenshot {
     private static final String TAG = "SCREENSHOT";
     private static final String AWS_SCREEN_DIRECTORY = "/test-screenshots/";
     private static final String SCREEN_DIRECTORY = "screenshots";
     private static final int SCREEN_SHOT_IMAGE_QUALITY = 100;
 
-    public static String getFilePath(Activity activity, String filename){
+    public static String getFilePath(Activity activity, String filename) {
         // the path might be /sdcard/Android/data/[app name]/files/screenshots/[filename].png
         return String.format("%s/%s.png",
                 activity.getExternalFilesDir(SCREEN_DIRECTORY).getAbsolutePath(),
@@ -43,42 +43,47 @@ public class ScreenShot {
                 UUID.randomUUID().toString()));
     }
 
-    private static void takeInternal(Activity activity, String fileName) {
-        File imageFile = new File(fileName);
+    private static void takeInternal(final Activity activity, final String fileName) {
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                File imageFile = new File(fileName);
 
-        // Verify that the directory exists and create it if not.
-        File directoryPath = imageFile.getParentFile();
-        if (!directoryPath.isDirectory()) {
-            Log.i(TAG, "Creating directory: " + directoryPath.toString());
-            if (!directoryPath.mkdirs()) {
-                Log.e(TAG, "Failed to create the directory");
-                return;
-            }
-        }
-
-        Log.i(TAG, "Saving to path: " + imageFile.toString());
-
-        View phoneView = activity.getWindow().getDecorView().getRootView();
-        phoneView.setDrawingCacheEnabled(true);
-        Bitmap bitmap = Bitmap.createBitmap(phoneView.getDrawingCache());
-        phoneView.setDrawingCacheEnabled(false);
-
-        OutputStream out = null;
-
-        try {
-            out = new FileOutputStream(imageFile);
-            bitmap.compress(Bitmap.CompressFormat.PNG, SCREEN_SHOT_IMAGE_QUALITY, out);
-            out.flush();
-        } catch (IOException e) {
-            Log.e(TAG, e.toString());
-        } finally {
-            try {
-                if (out != null) {
-                    out.close();
+                // Verify that the directory exists and create it if not.
+                File directoryPath = imageFile.getParentFile();
+                if (!directoryPath.isDirectory()) {
+                    Log.i(TAG, "Creating directory: " + directoryPath.toString());
+                    if (!directoryPath.mkdirs()) {
+                        Log.e(TAG, "Failed to create the directory");
+                        return;
+                    }
                 }
-            } catch (IOException e) {
-                Log.e(TAG, e.toString());
+
+                Log.i(TAG, "Saving to path: " + imageFile.toString());
+
+                View phoneView = activity.getWindow().getDecorView().getRootView();
+                phoneView.setDrawingCacheEnabled(true);
+                Bitmap bitmap = Bitmap.createBitmap(phoneView.getDrawingCache());
+                phoneView.setDrawingCacheEnabled(false);
+
+                OutputStream out = null;
+
+                try {
+                    out = new FileOutputStream(imageFile);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, SCREEN_SHOT_IMAGE_QUALITY, out);
+                    out.flush();
+                } catch (IOException e) {
+                    Log.e(TAG, e.toString());
+                } finally {
+                    try {
+                        if (out != null) {
+                            out.close();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, e.toString());
+                    }
+                }
             }
-        }
+        });
     }
 }
--- testapp/src/androidTest/java/com/swarmnyc/testapp/test/ScreenShotTest.java
+++ testapp/src/androidTest/java/com/swarmnyc/testapp/test/ScreenShotTest.java
@@ -1,11 +1,10 @@
 package com.swarmnyc.testapp.test;
 
 
-import android.os.Environment;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.swarmnyc.test.ScreenShot;
+import com.swarmnyc.test.Screenshot;
 import com.swarmnyc.testapp.MainActivity;
 
 import org.junit.Rule;
@@ -18,16 +17,17 @@ import java.util.UUID;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
-public class ScreenShotTest {
+public class ScreenshotTest {
     @Rule
     public ActivityTestRule<MainActivity> mainActivity = new ActivityTestRule<MainActivity>(MainActivity.class);
 
     @Test
-    public void takeTest(){
+    public void takeTest() throws InterruptedException {
         MainActivity activity = mainActivity.getActivity();
         String fileName = UUID.randomUUID().toString();
-        ScreenShot.take(activity, fileName);
+        Screenshot.take(activity, fileName);
+        Thread.sleep(100);
 
-        assertTrue((new File(ScreenShot.getFilePath(activity, fileName)).exists()));
+        assertTrue("The screenshot file didn't created", (new File(Screenshot.getFilePath(activity, fileName)).exists()));
     }
 }
